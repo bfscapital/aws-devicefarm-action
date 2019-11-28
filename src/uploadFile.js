033f69d0a0aa5da48bb78b62e544fec255c2e601
JD Lacey <jd@ironbound.io>
2019-11-28 00:39:49 -0400

    Update uploadFile.js

--- src/uploadFile.js
+++ src/uploadFile.js
@@ -2,10 +2,14 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 
 uploadFile = async (filePath, url) => {
+    const stats = fs.statSync()
+    console.log(`File stats: ${stats}`)
+    const fileSizeInBytes = stats.size 
     return fetch(url, {
         method: 'POST',
         headers: {
-            "Content-Type": 'application/octet-stream'
+            "Content-Type": 'application/octet-stream',
+            "Content-Length": fileSizeInBytes
         },
         body: fs.createReadStream(filePath)
     })
