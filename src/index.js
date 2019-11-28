366633913b4219641ed1b1013b3e522f6cfaaf33
JD Lacey <jd@ironbound.io>
2019-11-27 20:35:09 -0400

    Add app upload path

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const core = require('@actions/core')
 const AWS = require('aws-sdk')
 
@@ -28,11 +29,13 @@ const configAWS = () => {
 
 const run = async () => {
     try {
-        configAWS()
-
         const projectName = getInputWithDefault({ name: 'projectName', required: true })
         const devicePoolName = getInputWithDefault({ name: 'devicePoolName', required: true })
+        const appBinaryPath = getInputWithDefault({ name: 'appBinaryPath', required: true })
+        // const testPackagePath = getInputWithDefault({ name: 'testPackagePath', required: true })
+        // const testPackageType = getInputWithDefault({ name: 'testPackageType', required: true })
 
+        configAWS()
         const deviceFarm = new AWS.DeviceFarm()
 
         const projectResults = await deviceFarm.listProjects().promise()
@@ -42,18 +45,38 @@ const run = async () => {
             throw `Could not find a project with the name ${projectName}`
         }
 
-        const params = {
+        const devicePoolsParams = {
             arn: project.arn
         }
-        const devicePoolResults = await deviceFarm.listDevicePools(params).promise()
+        const devicePoolResults = await deviceFarm.listDevicePools(devicePoolsParams).promise()
         const devicePools = devicePoolResults.devicePools
         const devicePool = devicePools.find(({ name }) => name === devicePoolName)
         if (!devicePool) {
             throw `Could not find a device pool in project ${projectName} with the name ${devicePoolName}`
         }
 
+        const appBinaryFileExtension = path.extname(appBinaryPath).toLowerCase()
+        const appBinaryType = {
+            '.ipa': 'IOS_APP',
+            '.apk': 'ANDROID_APP'
+        }[appBinaryFileExtension]
+        if (!appBinaryType) {
+            throw `Invalid appBinaryPath file extension ${appBinaryFileExtension}. Must be ".ipa" or ".apk".`
+        }
+
+        const appBinaryName = path.basename(appBinaryPath)
+
+        const appUploadParams = {
+            name: appBinaryName,
+            projectArn: project.arn,
+            type: appBinaryType,
+            contentType: 'application/octet-stream'
+        }
+        const appUploadResults = await deviceFarm.createUpload(appUploadParams).promise()
+
         core.setOutput("project", JSON.stringify(project))
         core.setOutput("devicePool", JSON.stringify(devicePool))
+        core.setOutput("appUploadResults", JSON.stringify(appUploadResults))
 
     } catch (error) {
         core.setFailed(error.message)
